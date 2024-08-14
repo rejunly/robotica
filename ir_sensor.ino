b6e694bd030393851279f8415e36da4919ec4f74
Bela <139059248+rejunly@users.noreply.github.com>
2024-08-14 07:56:27 -0300

    Add files via upload

---
+++ ir_sensor.ino
@@ -0,0 +1,16 @@
+int pinoLed = 12; //PINO DIGITAL UTILIZADO PELO LED  
+int pinoSensor = 8; //PINO DIGITAL UTILIZADO PELO SENSOR
+   
+void setup(){  
+  pinMode(pinoSensor, INPUT); //DEFINE O PINO COMO ENTRADA
+  pinMode(pinoLed, OUTPUT); //DEFINE O PINO COMO SAÍDA   
+  digitalWrite(pinoLed, LOW); //LED INICIA DESLIGADO
+}  
+   
+void loop(){
+  if (digitalRead(pinoSensor) == LOW){ //SE A LEITURA DO PINO FOR IGUAL A LOW, FAZ
+        digitalWrite(pinoLed, HIGH); //ACENDE O LED
+  }else{//SENÃO, FAZ
+        digitalWrite(pinoLed, LOW); //apaga O LED
+  }    
+}
