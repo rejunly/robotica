d321c0285699fe50298c9af7223dcc3455e84668
Bela <139059248+rejunly@users.noreply.github.com>
2024-08-14 07:56:08 -0300

    Add files via upload

---
+++ distancia.ino
@@ -0,0 +1,41 @@
+// Definição dos pinos de conexão do sensor
+const int trigPin = 24;
+const int echoPin = 22;
+
+// Variáveis para armazenar a duração e a distância
+long duration;
+int distance;
+
+void setup() {
+  // Inicializa a comunicação serial
+  Serial.begin(9600);
+
+  // Define os modos dos pinos
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // Certifique-se de que o pino trig está baixo
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Envie um pulso alto de 10 microsegundos no pino trig
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Meça o tempo de duração do pulso no pino echo
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calcule a distância em centímetros, ajuste o fator de conversão se necessário
+  distance = duration * 0.0343 / 2; // ajuste da constante
+
+  // Exiba a distância medida no Monitor Serial
+  Serial.print("Distância: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // Aguarde 100ms antes de fazer a próxima medição
+  delay(100);
+}
